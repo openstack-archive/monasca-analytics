f695f22ecea3398f086d8ea22267b5070573e555
Andreas Jaeger <aj@suse.com>
2016-12-14 12:02:14 +0100

    Fix tools/test-setup.sh
    Replace {{ - as used in project-config - with {, we do not need
    quoting.
    
    This change is needed to fix gates now.
    
    Change-Id: Idea50d1f63e3076be68eed277c28141c311b2c97

--- tools/test-setup.sh
+++ tools/test-setup.sh
@@ -4,9 +4,9 @@
 # it sets up the test system as needed.
 # Developers should setup their test systems in a similar way.
 
-HOME=${{HOME:-/home/jenkins}}
+HOME=${HOME:-/home/jenkins}
 SPARK_DIR=$HOME/spark
-SPARK_VERSION=${{SPARK_VERSION:-1.6.1}}
+SPARK_VERSION=${SPARK_VERSION:-1.6.1}
 SPARK_TARBALL_NAME=spark-$SPARK_VERSION.tgz
 SPARK_URL=http://archive.apache.org/dist/spark/spark-$SPARK_VERSION/$SPARK_TARBALL_NAME
 
